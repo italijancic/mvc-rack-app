896b3352d0c1ed1292588513795a6a8506d5c64f
italijancic <italijancic@outlook.com>
2024-03-16 15:16:35 -0300

    Add erb renderong for static pages

--- app/controllers/base_controller.rb
+++ app/controllers/base_controller.rb
@@ -1,22 +1,16 @@
 # frozen_string_literal: true
 
 class BaseController
-  attr_reader :request
+  attr_reader :request, :resource, :action
 
   def initialize(request)
     @request = request
+    @resource = request.params[:resource].to_s
+    @action = request.params[:action].to_s
   end
 
   def index
-    build_response <<~HTML
-      <html>
-        <head><title>A Rack Demo</title></head>
-        <body>
-          <h1>This is the root page</h1>
-          <p>Hello from a controller!</p>
-        </body>
-      </html>
-    HTML
+    build_response(render_template)
   end
 
   def not_found(msg = '<h1>HTPP 404 Page not found :( !</h1>')
@@ -24,6 +18,26 @@ class BaseController
   end
 
   private
+
+  def render_template
+    template_file = File.join(resource, "#{action}.html.erb")
+    file_path = template_file_path_for(template_file)
+
+    if File.exist?(file_path)
+      puts "Rendering template file #{template_file}"
+      render_erb_file(file_path)
+    else
+      "ERROR: no available template file #{template_file}"
+    end
+  end
+
+  def template_file_path_for(file_name)
+    File.expand_path(File.join('../../views', file_name), __FILE__)
+  end
+
+  def render_erb_file(file_path)
+    File.read(file_path)
+  end
 
   def build_response(body, status: 200)
     [status, { 'Content-Type' => 'text/html' }, [body]]
